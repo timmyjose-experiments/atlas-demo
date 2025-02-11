b354f0b2c8d6163291688a6b3bb608fb67aa01e5
Timmy Jose <zoltan.jose@gmail.com>
2025-02-11 16:26:52 +0530

    chore: basic project setup.

---
+++ src/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button: {
+    height: 40,
+    width: '40%',
+    borderRadius: 5,
+    margin: 5,
+    backgroundColor: '#c0c0c0'
+  }
+})
